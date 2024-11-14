cb5503d5a3c7b8d7f3025e53c59a86843a43273d
Gufran04 <gufranahmad196@gmail.com>
2024-11-14 16:35:40 +0530

    javaQuestion

---
+++ AddtwoNum.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class AddtwoNum {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter num1: ");
+        int num1 = sc.nextInt();
+
+        System.out.print("enter num2: ");
+        int num2 = sc.nextInt();
+
+        int sum = num1 + num2;
+
+        System.out.println("Sume of two numbers is: " + sum);
+
+    }
+}
